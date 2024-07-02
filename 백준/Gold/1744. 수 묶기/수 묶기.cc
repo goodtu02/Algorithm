b35115975a19e326b7be47f37aaaea640d1819dc
goodtu02 <161540124+goodtu02@users.noreply.github.com>
2024-07-02 15:38:21 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.cc
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -2,49 +2,43 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
-#include <stack>
 
 #define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false)
 using namespace std;
 
 int main() {
   fast_io;
-  int k;
-  cin >> k;
-  vector<int> n; //음수
-  vector<int> p; //1보다 큰 양수
-  queue<int> negative; //음수
-  stack<int> positive; //1보다 큰 양수 정렬
+  int n;
+  cin >> n;
+  priority_queue<int, vector<int>, greater<>> negative; //음수 오름차순 정렬
+  priority_queue<int> positive; //양수 내림차순 정렬
+
   int zero = 0; //0
   int one = 0; //1
-  for (int i = 0; i < k; i++) {
+  for (int i = 0; i < n; i++) {
 	int a;
 	cin >> a;
-	if (a < 0) n.push_back(a);
+	if (a < 0) negative.push(a);
 	else if (a == 0) zero++;
 	else if (a == 1) one++;
-	else p.push_back(a);
+	else positive.push(a);
   }
   long long sum = 0;
-  sort(n.begin(), n.end());
-  for (int& i : n) negative.push(i);
-  sort(p.begin(), p.end());
-  for (int& i : p) positive.push(i);
-  while (negative.size() > 1) //두 음수 묶기 //원소가 없거나 1개 남으면 종료
+  while (negative.size() > 1) //두 음수 묶기
   {
-	int a = negative.front();
+	int a = negative.top();
 	negative.pop();
-	int b = negative.front();
+	int b = negative.top();
 	negative.pop();
 	sum += a * b;
   }
   while (zero > 0 && !negative.empty()) //처리 되지 않은 음수 0과 묶기
   {
-	if (negative.size() == 1 && negative.front() == 1) break;
+	if (negative.size() == 1 && negative.top() == 1) break;
 	negative.pop();
 	zero--;
   }
-  while (positive.size() > 1) //1보다 큰 두 양수 묶기 //원소가 없거나 1개 남으면 종료
+  while (positive.size() > 1) //1보다 큰 두 양수 묶기
   {
 	int a = positive.top();
 	positive.pop();
@@ -55,7 +49,7 @@ int main() {
   int ps = negative.size();
   int ms = positive.size();
   for (int i = 0; i < ps; i++) {
-	sum += negative.front();
+	sum += negative.top();
 	negative.pop();
   }
   for (int i = 0; i < ms; i++) {
