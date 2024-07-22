3c44c03f5517c2b4cd5d897ef8db0307fff556e4
goodtu02 <161540124+goodtu02@users.noreply.github.com>
2024-07-22 19:46:54 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false)
+
+using namespace std;
+
+int main() {
+  fast_io;
+  cout << "\\    /\\" << '\n';
+  cout << " )  ( ')" << '\n';
+  cout << "(  /  )" << '\n';
+  cout << " \\(__)|";
+
+  return 0;
+}
