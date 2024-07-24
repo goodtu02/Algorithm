d038714418f7a19f6c3aebe3274b6b5c65ba9b80
goodtu02 <161540124+goodtu02@users.noreply.github.com>
2024-07-24 14:17:15 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false)
+using namespace std;
+
+vector<int> city;
+vector<int> change;
+
+int find(int x) {
+    if (city[x] == x) return x; // 대표 노드 반환
+    change.push_back(x);
+    return find(city[x]);
+}
+
+void union_(int x, int y) {
+    //대표 노드 합치기
+    change.clear();
+    x = find(x);
+    y = find(y);
+    city[x] = min(x, y);
+    city[y] = min(x, y);
+    for (int i: change) city[i] = min(x, y);
+}
+
+int main() {
+    fast_io;
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) city.push_back(i);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int a;
+            cin >> a;
+            if (a == 1) union_(i, j);
+        }
+    }
+    vector<int> schedule;
+    for (int i = 0; i < m; i++) {
+        int a;
+        cin >> a;
+        schedule.push_back(a - 1);
+    }
+    for (int i = 0; i < schedule.size() - 1; i++) {
+        if (find(schedule[i]) != find(schedule[i + 1])) {
+            //같은 집합 확인
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+    return 0;
+}
